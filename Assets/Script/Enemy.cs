b4827c4d0df36f383819d0914865f2b02d0d9602
MarioLDD <112513242+MarioLDD@users.noreply.github.com>
2022-11-05 04:13:33 -0300

    trabajando sobre random

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEngine.GraphicsBuffer;
 
 public class Enemy : MonoBehaviour
 {
     private Animator playerAnim;
     public NavMeshAgent navigation;
     public Transform target;
+    public int i = 1;
 
 
     public Transform a;
@@ -16,20 +18,23 @@ public class Enemy : MonoBehaviour
     public Transform c;
     public Transform d;
 
+    public GameObject randomActivador;
+    string casa;
+
 
     void Start()
     {
         playerAnim = GetComponent<Animator>();
         navigation = GetComponent<NavMeshAgent>();
-        navigation.destination = target.position;
+        // navigation.destination = target.position;
 
-
+        InvokeRepeating("RandomFunction", 2f, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
         // Debug.Log(navigation.desiredVelocity.magnitude);       velocidad definida
         //  Debug.Log(navigation.speed);  velocidad definida
 
@@ -42,7 +47,7 @@ public class Enemy : MonoBehaviour
         }
         else
         {
-            playerAnim.SetFloat("Speed_f", 0f);
+          //  playerAnim.SetFloat("RandomFunction", 0f);
         }
 
 
@@ -51,79 +56,125 @@ public class Enemy : MonoBehaviour
             playerAnim.SetFloat("Speed_f", 0.26f);
             playerAnim.SetBool("Static_b", true);
         }
-
+        /*
         void Run()
         {
             playerAnim.SetFloat("Speed_f", 0.51f);
             playerAnim.SetBool("Static_b", true);
         }
-
+        */
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        float oA = Vector3.Distance(transform.position, a.transform.position);
-        float oB = Vector3.Distance(transform.position, b.transform.position);
-        float oC = Vector3.Distance(transform.position, c.transform.position);
-        float oD = Vector3.Distance(transform.position, d.transform.position);
-
-        if (oA < oC)
+        if (collision.gameObject.CompareTag("Bomb"))
         {
-            if (oB < oD)
+
+            casa = randomActivador.GetComponent<RandomActivadorScript>().EnemyToTarget();
+            Debug.Log(casa);
+
+            /*
+            float oA = Vector3.Distance(transform.position, a.transform.position);
+            float oB = Vector3.Distance(transform.position, b.transform.position);
+            float oC = Vector3.Distance(transform.position, c.transform.position);
+            float oD = Vector3.Distance(transform.position, d.transform.position);
+
+            if (oA < oC)
             {
-                if (oA < oB)
+                if (oB < oD)
                 {
-                    //Debug.Log("cerca de A");
-                    navigation.SetDestination(new Vector3(transform.position.x, transform.position.y, a.position.z - 15));
+                    if (oA < oB)
+                    {
+                        //Debug.Log("cerca de A");
+                        navigation.SetDestination(new Vector3(transform.position.x, transform.position.y, a.position.z - 15));
+                    }
+                    else
+                    {
+                        //Debug.Log("cerca de B");
+                        navigation.SetDestination(new Vector3(b.position.x - 15, transform.position.y, transform.position.z));
+                    }
                 }
                 else
                 {
-                    //Debug.Log("cerca de B");
-                    navigation.SetDestination(new Vector3(b.position.x - 15, transform.position.y, transform.position.z));
+                    if (oA < oD)
+                    {
+                        //Debug.Log("cerca de A");
+                        navigation.SetDestination(new Vector3(transform.position.x, transform.position.y, a.position.z - 15));
+                    }
+                    else
+                    {
+                        //Debug.Log("cerca de D");
+                        navigation.SetDestination(new Vector3(d.position.x + 15, transform.position.y, transform.position.z));
+                    }
                 }
             }
             else
             {
-                if (oA < oD)
+                if (oB < oD)
                 {
-                    //Debug.Log("cerca de A");
-                    navigation.SetDestination(new Vector3(transform.position.x, transform.position.y, a.position.z - 15));
+                    if (oC < oB)
+                    {
+                        //Debug.Log("cerca de C");
+                        navigation.SetDestination(new Vector3(transform.position.x, transform.position.y, c.position.z + 15));
+                    }
+                    else
+                    {
+                        //Debug.Log("cerca de B");
+                        navigation.SetDestination(new Vector3(b.position.x - 15, transform.position.y, transform.position.z));
+                    }
                 }
                 else
                 {
-                    //Debug.Log("cerca de D");
-                    navigation.SetDestination(new Vector3(d.position.x + 15, transform.position.y, transform.position.z));
+                    if (oC < oD)
+                    {
+                        //Debug.Log("cerca de C");
+                        navigation.SetDestination(new Vector3(transform.position.x, transform.position.y, c.position.z + 15));
+                    }
+                    else
+                    {
+                        //Debug.Log("cerca de D");
+                        navigation.SetDestination(new Vector3(d.position.x + 15, transform.position.y, transform.position.z));
+                    }
                 }
             }
+            */
+
+        }
+
+        if (collision.gameObject.CompareTag("Bounds"))
+        {
+            Debug.Log("choco");
+            Destroy(gameObject);
+
+        }
+    }
+
+    void RandomFunction()
+    {
+       
+
+        if (i != 5)
+        {
+
+            navigation.destination = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
+            Debug.Log(i);
+            i++;
+
         }
         else
         {
-            if (oB < oD)
-            {
-                if (oC < oB)
-                {
-                    //Debug.Log("cerca de C");
-                    navigation.SetDestination(new Vector3(transform.position.x, transform.position.y, c.position.z + 15));
-                }
-                else
-                {
-                    //Debug.Log("cerca de B");
-                    navigation.SetDestination(new Vector3(b.position.x - 15, transform.position.y, transform.position.z));
-                }
-            }
-            else
-            {
-                if (oC < oD)
-                {
-                    //Debug.Log("cerca de C");
-                    navigation.SetDestination(new Vector3(transform.position.x, transform.position.y, c.position.z + 15));
-                }
-                else
-                {
-                    //Debug.Log("cerca de D");
-                    navigation.SetDestination(new Vector3(d.position.x + 15, transform.position.y, transform.position.z));
-                }
-            }
+            CancelInvoke();
+            Debug.Log("estoy fuera");
         }
+
     }
+
+
+
+
+
+
 }
+
+
+
---
+++ Assets/Script/RandomActivadorScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomActivadorScript : MonoBehaviour
+{
+    public string m = "tito";
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string EnemyToTarget()
+    {
+        Debug.Log("holaaaaa");
+
+
+    return m = "fito";
+
+
+
+    }
+}
